f827850c3d705d8b34bdac1695f8369d64cab661
m0j0hn <m0j0hn@users.noreply.github.com>
2020-12-18 13:45:52 -0500

    Add SHA

--- Formula/asciidoctor-diagram.rb
+++ Formula/asciidoctor-diagram.rb
@@ -1,18 +1,20 @@
 class AsciidoctorDiagram < Formula
-  desc "Asciidoctor Diagram is a set of Asciidoctor extensions that enable you to add diagrams, which you describe using plain text, to your AsciiDoc document."
-  homepage "https://asciidoctor.org/"
-  url "https://github.com/asciidoctor/asciidoctor-diagram/archive/v1.5.10.tar.gz"
-  sha256 "77051e3b203a8782a229d330e9248703c22af992b80e17caf77ae79fb1c8aaf5"
+  desc "Asciidoctor Diagram is a set of Asciidoctor extensions that enable you to add diagrams, which you describe using plain text, to your AsciiDoc document"
+  homepage "https://asciidoctor.org/docs/asciidoctor-diagram"
+  url "https://github.com/asciidoctor/asciidoctor-diagram/archive/v2.0.5.tar.gz"
+  sha256 "3faf4d3b3b8ad139864221c1c7a1bd853ce3f7f269852cbd0378e47d6f0b5403"
   license "MIT"
 
   depends_on "ruby" if MacOS.version <= :sierra
 
+  #depends_on "asciidoctor" => :recommended
+
   bottle do
     cellar :any_skip_relocation
-    sha256 "4af4798f8081100713a1b3d301107b5ddd01d1f85d40d5f351d12b3261148fbe" => :big_sur
-    sha256 "b6d75bed00d6ab5586634823bee006e5f0bb3c57f9f46317b675c33b28eb7552" => :catalina
-    sha256 "8ce4eb3ad0b311775a31f15d32939df21f3eefbac6dc39ac76f2d2573920b5af" => :mojave
-    sha256 "d4fa41fc1f142f4d8ad25c2063ed79dd04091386d87c7996c17c9adcb10be301" => :high_sierra
+    #sha256 "4af4798f8081100713a1b3d301107b5ddd01d1f85d40d5f351d12b3261148fbe" => :big_sur
+    #sha256 "b6d75bed00d6ab5586634823bee006e5f0bb3c57f9f46317b675c33b28eb7552" => :catalina
+    #sha256 "8ce4eb3ad0b311775a31f15d32939df21f3eefbac6dc39ac76f2d2573920b5af" => :mojave
+    #sha256 "d4fa41fc1f142f4d8ad25c2063ed79dd04091386d87c7996c17c9adcb10be301" => :high_sierra
   end
 
   #resource "concurrent-ruby" do
@@ -116,8 +118,8 @@ class AsciidoctorDiagram < Formula
   #end
 
   resource "asciidoctor-diagram" do
-    url "https://rubygems.org/gems/asciidoctor-diagram-1.5.10.gem"
-    #sha256 "3e70d0e513f4d631a4b667fed634700d93b104e0d3b4c33f993979df6a67d3f1"
+     url "https://github.com/asciidoctor/asciidoctor-diagram/archive/v2.0.5.tar.gz"
+     sha256 "3faf4d3b3b8ad139864221c1c7a1bd853ce3f7f269852cbd0378e47d6f0b5403"
   end
 
   #resource "asciidoctor-pdf" do
@@ -142,15 +144,31 @@ class AsciidoctorDiagram < Formula
       system "gem", "install", r.cached_download, "--ignore-dependencies",
              "--no-document", "--install-dir", libexec
     end
+
     #system "gem", "build", "asciidoctor.gemspec"
     #system "gem", "install", "asciidoctor-#{version}.gem"
-    system "gem", "build", "asciidoctor-diagram.gemspec"
-    system "gem", "install", "asciidoctor-diagram-#{version}.gem"
+
+    system "pwd"
+    system "ls", "-l"
+
+    #system "gem", "build", "asciidoctor-diagram.gemspec"
+    #system "gem", "install", "asciidoctor-diagram-#{version}.gem"
+    #system "gem", "install", "asciidoctor-diagram.gem"
+    #system "gem", "install", "asciidoctor-diagram"
+
     #bin.install Dir[libexec/"bin/asciidoctor"]
     #bin.install Dir[libexec/"bin/asciidoctor-pdf"]
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+    #bin.install Dir[libexec/"bin/*"]
+    #bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
     #man1.install_symlink "#{libexec}/gems/asciidoctor-#{version}/man/asciidoctor.1" => "asciidoctor.1"
+
+    # COPIED FROM swiftgen.rb:
+    # Install bundler, then use it to `rake cli:install` SwiftGen
+    #ENV["GEM_HOME"] = buildpath/"gem_home"
+    #system "gem", "install", "bundler"
+    #ENV.prepend_path "PATH", buildpath/"gem_home/bin"
+    #system "bundle", "install", "--without", "development", "release"
+    #system "bundle", "exec", "rake", "cli:install[#{bin},#{lib},#{pkgshare}/templates]"
   end
 
   test do
